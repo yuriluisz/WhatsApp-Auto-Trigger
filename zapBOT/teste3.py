61e539bc3489a71581f527f469ef7775698f2f59
yuriluisz <71914185+yuriluisz@users.noreply.github.com>
2022-11-18 16:47:26 -0400

    Mensagens predefinidas gg ez

--- zapBOT/teste3.py
+++ zapBOT/teste3.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import ttk
+import pywhatkit as kit
 
 
 janela = Tk()
@@ -10,7 +11,7 @@ def macaco(event):
     brabo = int(lsbx.curselection()[0])
     monk = lsbx.get(brabo)
     print(monk)
-
+    kit.sendwhatmsg_instantly("55_65999532826", monk)
 
 lsbx = Listbox(janela, height=10, width=50)
 lsbx.grid(column=0, row=0, padx=5, pady=5)
--- zapBOT/teste4.py
+++ zapBOT/teste4.py
@@ -2,57 +2,86 @@ import pywhatkit as pk
 from tkinter import *
 from tkinter import ttk
 
-def EnviarMSG(event):
-    try:
-        num = inputnum.get("1.0", END)
-        if inputmsg.get("1.0", END) == "":
+
+# Def do botao de enviar mensagem selecionada
+def msgpronta():
+    for i in listbox.curselection():
+        try:
+            num = inputnum.get("1.0", END)
             brabo = int(listbox.curselection()[0])
             msgzz = listbox.get(brabo)
             print(msgzz)
             pk.sendwhatmsg_instantly(f"{num}", msgzz)
             status.config(text="Status: Mensagem Enviada!")
-        else:
-            msgzz = inputmsg.get("1.0", END)
-            pk.sendwhatmsg_instantly(f"{num}", msgzz)
-            status.config(text="Status: Mensagem Enviada!")
+        except:
+            status.config(text="Status: Erro no Envio! verifique o numero.")
+
+# Def do botao de enviar mensagem escrita
+def EnviarMSG():
+    try:
+        num = inputnum.get("1.0", END)
+        msgzz = inputmsg.get("1.0", END)
+        pk.sendwhatmsg_instantly(f"{num}", msgzz)
+        status.config(text="Status: Mensagem Enviada!")
     except:
         status.config(text="Status: Erro no Envio! verifique o número.")
 
+def adicionar():
+    print("mae?")
+
 
 # Mensagens
-msg1 = "Olá, logo logo entraremos em contato!"
-msg2 = "Olá, No momento estamos indisponiveis, entraremos em contato o mais rapido possivel em breve"
-msg3 = "Olá, E um prazer atender-lo como posso ajudar"
+msg1 = "Olá, Logo Logo vamos te responder!"
+msg2 = "Olá, No momento estamos indisponiveis, entraremos em contato o mais rapido possivel."
+msg3 = "Olá, E um prazer atender-lo como posso ajudar?"
+msg4 = "Olá, No momento não estamos atendendo."
 
+mensagens = [msg1, msg2, msg3, msg4]
+
+# Interface
 janela = Tk()
 janela.title("WhatsApp auto message")
 janela.resizable(False, False)
 
+# Inserir Numero de telefone
 num = Label(janela, text='Insira o nmero de telefone e coloque um "_" apos o DDD ex:55_65...')
 num.grid(column=0, row=0, padx=10, pady=0)
 
 inputnum = Text(janela, height=1, width=40)
 inputnum.grid(column=0, row=1, padx=10, pady=10)
 
+# Inserir Mensagem
 msg = Label(janela, text="Insira a mensagem que deseja enviar:")
 msg.grid(column=0, row=2, padx=10, pady=10)
 
 inputmsg = Text(janela, height=5, width=40)
 inputmsg.grid(column=0, row=3, padx=10, pady=10)
 
-listbox = Listbox(janela, height=5)
+# Escolher mensagem predefinida
+listbox = Listbox(janela, height=5, width=40)
 listbox.insert(END, msg1)
 listbox.insert(END, msg2)
 listbox.insert(END, msg3)
+listbox.insert(END, msg4)
 listbox.grid(column=1, row=3, padx=10, pady=10)
-listbox.bind("<<ListboxSelect>>", EnviarMSG)
 
+# Status do Envio
 status = Label(janela, text="Status:")
-status.grid(column=0,row=4, padx=10, pady=10)
+status.grid(column=1,row=0, padx=10, pady=10)
 
-Enviar = Button(janela, text="Enviar", command=EnviarMSG)
-Enviar.grid(column=0, row=5, padx=10, pady=10)
+# Botao Enviar mensagem pronta
+Enviarpronta = Button(janela, text="Enviar mensagem selecionada", command=msgpronta)
+Enviarpronta.grid(column=1, row=4, padx=10, pady=10)
 
+# Botao Enviar mensagem escrita
+Enviar = Button(janela, text="Enviar mensagem escrita", command=EnviarMSG)
+Enviar.grid(column=0, row=4, padx=10, pady=10)
+
+# Botao Adicionar mensagem escrita
+adc = Button(janela, text="Adicionar Mensagem", command=adicionar)
+adc.grid(column=0, row=5, padx=10, pady=10)
+
+# Botao Sair
 sair = Button(janela, text="Sair", command=janela.destroy)
 sair.grid(column=1, row=5, padx=10, pady=10)
 
