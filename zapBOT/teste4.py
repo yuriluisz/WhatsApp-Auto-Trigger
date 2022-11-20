2eb447ec61d269f3eae94bcc54447578de8e5f2e
Yuri Luis <71914185+yuriluisz@users.noreply.github.com>
2022-11-20 08:07:38 -0400

    Finalizado?
    Adicionado botao como usar
    e finalmente adicionado o tao pedido pelo professor o botao para enviar documentos (que so envia fotos 1 por vez inclusive mas e o melhor q eu posso fazer) amem.

--- zapBOT/teste4.py
+++ zapBOT/teste4.py
@@ -1,6 +1,8 @@
 import pywhatkit as pk
 from tkinter import *
 from tkinter import ttk
+from tkinter import filedialog
+from tkinter.messagebox import showinfo
 
 
 # Def do botao de enviar mensagem selecionada
@@ -26,8 +28,24 @@ def EnviarMSG():
     except:
         status.config(text="Status: Erro no Envio! verifique o número.")
 
-def adicionar():
-    print("mae?")
+# Def do botao de escolher imagem
+def fndimg():
+    try:
+        num = inputnum.get("1.0", END)
+        msgzz = inputmsg.get("1.0", END)
+        imge = filedialog.askopenfilename(initialdir="/", title="Selecione UMA imagem", filetypes=(("png","*.png*"),("jpg","*jpg*"),("Todos","*.*")))
+        print(imge)
+        pk.sendwhats_image(f"{num}",imge, msgzz)
+    except:
+        status.config(text="Status: Erro no Envio! verifique o número.")
+
+# Def do botao de Como usar?
+def hwtuse():
+    showinfo(
+        title="Como usar?",
+        message="Numero: Sempre que for inserir o numero coloque da seguinte maneira 55_65xxxxxxxx com um _ depois do DDD \n Imagens: Imagens precisam ter o numero já inserido, caso contrario nao sera enviada, pois o programa envia automaticamente apos escolher a imagem. Também é possivel enviar imagens com mensagens, basta escrever a mensagem e escolher a imagen (nao funciona com mensagem pronta)"
+    )
+    
 
 
 # Mensagens
@@ -36,7 +54,7 @@ msg2 = "Olá, No momento estamos indisponiveis, entraremos em contato o mais rap
 msg3 = "Olá, E um prazer atender-lo como posso ajudar?"
 msg4 = "Olá, No momento não estamos atendendo."
 
-mensagens = [msg1, msg2, msg3, msg4]
+mensagens = [msg1, msg2, msg3, msg4,]
 
 # Interface
 janela = Tk()
@@ -44,25 +62,24 @@ janela.title("WhatsApp auto message")
 janela.resizable(False, False)
 
 # Inserir Numero de telefone
-num = Label(janela, text='Insira o nmero de telefone e coloque um "_" apos o DDD ex:55_65...')
+num = Label(janela, text='Insira o numero de telefone:')
 num.grid(column=0, row=0, padx=10, pady=0)
 
 inputnum = Text(janela, height=1, width=40)
 inputnum.grid(column=0, row=1, padx=10, pady=10)
 
 # Inserir Mensagem
-msg = Label(janela, text="Insira a mensagem que deseja enviar:")
+msg = Label(janela, text="Insira a mensagem:")
 msg.grid(column=0, row=2, padx=10, pady=10)
 
 inputmsg = Text(janela, height=5, width=40)
 inputmsg.grid(column=0, row=3, padx=10, pady=10)
 
 # Escolher mensagem predefinida
-listbox = Listbox(janela, height=5, width=40)
-listbox.insert(END, msg1)
-listbox.insert(END, msg2)
-listbox.insert(END, msg3)
-listbox.insert(END, msg4)
+listvar = StringVar()
+listbox = Listbox(janela, listvariable=listvar, height=5, width=40)
+for msgs in mensagens:
+    listbox.insert(END, msgs)
 listbox.grid(column=1, row=3, padx=10, pady=10)
 
 # Status do Envio
@@ -77,13 +94,17 @@ Enviarpronta.grid(column=1, row=4, padx=10, pady=10)
 Enviar = Button(janela, text="Enviar mensagem escrita", command=EnviarMSG)
 Enviar.grid(column=0, row=4, padx=10, pady=10)
 
-# Botao Adicionar mensagem escrita
-adc = Button(janela, text="Adicionar Mensagem", command=adicionar)
+# Botao Como usar?
+adc = Button(janela, text="Como usar?", command=hwtuse)
 adc.grid(column=0, row=5, padx=10, pady=10)
 
 # Botao Sair
 sair = Button(janela, text="Sair", command=janela.destroy)
 sair.grid(column=1, row=5, padx=10, pady=10)
+
+# Botao para escolher uma imagem
+slcimg = Button(janela, text="Escolher imagem", command=fndimg)
+slcimg.grid(column=1, row=1, padx=10, pady=10)
 
 
 janela.mainloop()
---
+++ zapBOT/teste5.py
@@ -0,0 +1,7 @@
+import pywhatkit as kit
+
+path = r'C:\Users\yulus\Downloads\c69310fe-8767-4386-a086-78270057749a.jpg'
+sexop = path.replace("'", "")
+sexzz = sexop.replace("r", "")
+print(sexzz)
+kit.sendwhats_image("55_65999532826", "C:\\Users\\yulus\\Downloads\\c69310fe-8767-4386-a086-78270057749a.jpg")
